54d2fc13536d4ec6ffadbf6c9bb21d83f1a65ed4
unknown <louis.kleverman@gmail.com>
2018-08-27 22:26:35 +0200

    last push before hand in

--- test/TestWarcraftCoin.sol
+++ test/TestWarcraftCoin.sol
@@ -7,6 +7,7 @@ import "truffle/DeployedAddresses.sol";
 contract TestWarcraftCoin{
     WarcraftCoin warcraftCoin;
 
+    
     function testCorrectDeploymentOwner() public{
 
         warcraftCoin = new WarcraftCoin();
@@ -20,30 +21,6 @@ contract TestWarcraftCoin{
         uint expected = 10000000000000;
         
         uint got = warcraftCoin.getBalanceOf(address(this));
-
-        Assert.equal(got, expected, "Not the right quantity");
-    }
-    function testBalanceBeforeGiving() public{
-        address a = address(warcraftCoin);
-        uint expected = 0;
-        
-        uint got = warcraftCoin.getBalanceOf(a);
-
-        Assert.equal(got, expected, "Not the right quantity");
-    }
-    function testGiveToken() public{
-        warcraftCoin.giveToken(address(warcraftCoin),50);
-        uint expected = 50;
-        
-        uint got = warcraftCoin.getBalanceOf(warcraftCoin);
-
-        Assert.equal(got, expected, "Not the right quantity");
-    }
-
-    function testCorrectBalanceAfterGive() public{
-        uint expected = 10000000000000 - 50;
-        
-        uint got = warcraftCoin.getBalanceOf(this);
 
         Assert.equal(got, expected, "Not the right quantity");
     }
